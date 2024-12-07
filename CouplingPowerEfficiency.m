f7a6c62d0e16429f0478bec713e2061c9f2e5024
gracedavanzo <gracedavanzo@Graces-Mac-Studio.local>
2024-12-07 02:43:08 -0500

    Stuff

--- CouplingPowerEfficiency.m
+++ CouplingPowerEfficiency.m
@@ -1,6 +1,8 @@
 function [powerPercent] = CouplingPowerEfficiency(mode_width_i,mode_width_t)
 %COUPLINGPOWEREFFICIENCY Summary of this function goes here
 %   Detailed explanation goes here
-powerPercent = 4*(((mode_width_i^2)*(mode_width_t^2))/(mode_width_i^2+mode_width_t^2)^2);
+num = (mode_width_i^2)*(mode_width_t^2);
+den = (mode_width_i^2+mode_width_t^2)^2;
+powerPercent = 4*(num/den);
 end
 
--- Main.m
+++ Main.m
@@ -28,9 +28,10 @@ E_0 = sqrt(laserPower*(miu_0/laserAvgWaveLength)*sqrt(2/((x0^2)*pi)));
 %To find variables that allow us to calculate mode width, which lets use
 %calc power coupling efficiency
 b= 0.0017;
-
+%b= 0.856;
 V = 2*atan(sqrt(b/(1-b)))/sqrt(1-b);
 h_taper = V/(k0*sqrt(nf^2-ns^2)); %nm
+h_taper_saved = 6.23e-9; %nm 
 %gamma = k0*sqrt(b*sqrt(nf^2-ns^2))
 beta = k0*sqrt((nf^2-ns^2)*b+ns^2);
 gamma = sqrt(beta^2-(k0^2)*(ns^2));
@@ -50,16 +51,23 @@ powerTransmitted = CouplingPowerEfficiency(1/gamma,x0)
 claddingDistance = 3e-6;
 total = 2*sin(kappa*h_taper/2)/cos(kappa*h_taper/2)+(1/gamma);
 outsideOfMain = exp(claddingDistance)/(2*gamma);
-claddingLoss = (outsideOfMain)/(total)
-
+claddingLoss = (outsideOfMain)/(total);
+modes = V/pi;
 %Aim is to have the balance of the two above numbers be ideal so we get
 %certain percent of power
 
 %Get normalized amplitude
 
 
+h_taper_saved = 6.23e-9; %nm 
+
+%Next increase width
 
 %Beam Splitter
+powerAfterTransmission = laserPower*powerTransmitted - laserPower*powerTransmitted*claddingLoss;
+b = 0.856;
+hbeam = 0.47e-6; %microns
+
 PowerPercentage1 = 1;
 PowerPercentage2 = 0;
 PowerPercentage3 = 0;
@@ -79,12 +87,45 @@ lengthNext2 = 1;
 %Third pairing 2-4
 
 
-%Integral for non-degenerate coupling - chang around to solve for
-%amplitude?
-%insert waveguide functions
-%
-%Check total area of waveguide
+%Integral for non-degenerate coupling - change around to solve for
+V = hbeam * k0 * sqrt(nf^2-ns^2);
+neff1 = beta/k0;
+length = 1e-3; %length of waveguides
 
+%Check total area of waveguide
+%Decided two microns was sufficent
+hmax=2e-6;
+Vmax=hmax*k0*sqrt(nf^2-ns^2)
+
+V2 = 3*pi;
+h2 = V2/(k0*sqrt(nf^2-ns^2));
+syms b
+eqn = V2==2*atan(sqrt(b/(1-b)))/sqrt(1-b);
+b = vpasolve(eqn,b);
+b=double(b)
+beta2 = k0*sqrt((nf^2-ns^2)*b+ns^2);
+neff2 = beta2/k0;
+
+V3 = 5*pi;
+h3 = V3/(k0*sqrt(nf^2-ns^2));
+syms b
+eqn = V3==2*atan(sqrt(b/(1-b)))/sqrt(1-b);
+b = vpasolve(eqn,b);
+b=double(b);
+beta3 = k0*sqrt((nf^2-ns^2)*b+ns^2);
+neff3 = beta3/k0;
+
+V4 = 7*pi;
+h4 = V3/(k0*sqrt(nf^2-ns^2));
+syms b
+eqn = V4==2*atan(sqrt(b/(1-b)))/sqrt(1-b);
+b = vpasolve(eqn,b);
+b=double(b);
+beta4 = k0*sqrt((nf^2-ns^2)*b+ns^2);
+neff4 = beta4/k0;
+
+modes=V/pi
+Area = 4*(length*hmax);
 %Use to get amplitude needed to get correct amplitude, 
 % which then reverse to get width of original wave guide?
 
